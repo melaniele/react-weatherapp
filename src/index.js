c2e6e2fda03101716c50a3db61237a41900fb6b0
Melanie Le <77984641+melaniele@users.noreply.github.com>
2022-02-27 14:23:51 -0500

    css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* BTI425 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Thi Hong Minh Le (Melanie) - Student ID: 103087201  Date: February 26th, 2022
+* Heroku Link: https://react-weatherapp-melanie.herokuapp.com/
+*
+********************************************************************************/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routing from './Routing'
